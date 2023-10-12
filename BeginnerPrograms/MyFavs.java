eb07d8c021c17341c1920616cb120085401597e1
emluloh <127991247+emluloh@users.noreply.github.com>
2023-10-11 20:00:11 -0500

    Add files via upload

---
+++ BeginnerPrograms/MyFavs.java
@@ -0,0 +1,22 @@
+// My Favs - Java 1A
+// Eileen Luloh - Sum '23
+
+
+public class MyFavs
+{
+   public static void main (String [] args)
+   {
+      String movie, car;
+      double carCost;
+      int gradYear;
+      
+      movie = "Stargate";
+      car = "Mater from Cars";
+      carCost = 4.99;
+      gradYear = 2024;
+      
+      System.out.println("My favorite movie is " + movie);
+      System.out.println("My favorite car is " + car + " and costs " + carCost);
+      System.out.println("I graduate in " + gradYear);
+   }
+}
