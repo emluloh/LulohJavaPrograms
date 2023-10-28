37c907b22da6d8a3301e4bbfb07c08b681f89544
emluloh <127991247+emluloh@users.noreply.github.com>
2023-10-28 18:23:51 -0500

    Updated Oct. 28, 2023

---
+++ Level 3/DelphiSurvey/28Oct2023/Issue.java
@@ -0,0 +1,21 @@
+
+
+public class Issue 
+{
+    private int value; 
+
+    public Issue(int value) 
+    {
+        this.value = value;
+    }
+
+    public int getValue() 
+    {
+        return value;
+    }
+
+    public void setValue(int value) 
+    {
+        this.value = value;
+    }
+}
