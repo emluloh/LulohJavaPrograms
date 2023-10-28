36c18d082964422c7ab50a0a34c6bfd15381aab7
emluloh <127991247+emluloh@users.noreply.github.com>
2023-10-28 18:23:10 -0500

    Updated Oct. 28, 2023

---
+++ Level 3/DelphiSurvey/28Oct2023/DelphiMethod.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DelphiMethod 
+{
+    private List<Round> rounds;
+
+    public DelphiMethod() 
+    {
+        this.rounds = new ArrayList<>();
+    }
+
+    public void addRound(Round round) 
+    {
+        rounds.add(round);
+    }
+
+    public Round getRound(int index) 
+    {
+        return rounds.get(index);
+    }
+
+    public int getNumberOfRounds() 
+    {
+        return rounds.size();
+    }
+}
