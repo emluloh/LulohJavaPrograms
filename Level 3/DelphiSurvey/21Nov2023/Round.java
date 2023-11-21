9000a5b68e951599735d8fe67caa0ba04dd05110
emluloh <127991247+emluloh@users.noreply.github.com>
2023-11-21 03:50:01 -0600

    Create Round.java

---
+++ Level 3/DelphiSurvey/21Nov2023/Round.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Round 
+{
+    private List<Issue> issues;
+    
+    public Round()
+    {
+        this.issues = new ArrayList<>();
+    }
+    
+    public void addIssue(Issue issue) 
+    {
+        issues.add(issue);
+    }
+
+    public Issue getIssue(int index) 
+    {
+        return issues.get(index);
+    }
+    
+    public List<Issue> getIssues()
+    {
+        return issues;
+    }
+
+    public int getNumberOfIssues() 
+    {
+        return issues.size();
+    }
+    
+}
